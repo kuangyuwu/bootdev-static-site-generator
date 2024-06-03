721694d0a3eff8efc08804f2e1f2ca425f967a8d
kuangyuwu <kuangyuwu95@gmail.com>
2024-06-03 16:33:38 -0500

    add textnode

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
+from textnode import Textnode
+
+def main() -> None:
+    node = Textnode("This is a text node", "bold", "https://www.boot.dev")
+    print(node)
+
 if __name__ == '__main__':
-    print("hello world")
+    main()

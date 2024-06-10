f783bf440cc8d4e6bf022035358e11d887b17f12
kuangyuwu <kuangyuwu95@gmail.com>
2024-06-10 12:21:56 -0500

    combile files htmlnode and leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,10 +18,32 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
-        
+    
+class LeafNode(HTMLNode):
+
+    def __init__(self, value, tag=None, props=None) -> None:
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        return (
+            self.value
+            if self.tag is None
+            else f'<{self.tag}{self.props_to_html()}>{self.value}<\\{self.tag}>'
+        )
+
+    def __repr__(self) -> str:
+        return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
+
+
 if __name__ == '__main__':
     node = HTMLNode("test", "test", "test", {"test": "test", "test2": "test2"})
     print(node)
     print(node.props_to_html())
     node2 = HTMLNode()
     print(node2.props_to_html())
+
+    node = LeafNode("test text 123", "p", {"test": "test", "test2": "test2"})
+    print(node)
+    print(node.to_html())
+    node2 = LeafNode("test text 123")
+    print(node2.to_html())
--- src/leafnode.py
+++ src/leafnode.py
@@ -1,23 +1,23 @@
-from .htmlnode import HTMLNode
+# from .htmlnode import HTMLNode
 
-class LeafNode(HTMLNode):
+# class LeafNode(HTMLNode):
 
-    def __init__(self, value, tag=None, props=None) -> None:
-        super().__init__(tag=tag, value=value, props=props)
+#     def __init__(self, value, tag=None, props=None) -> None:
+#         super().__init__(tag=tag, value=value, props=props)
 
-    def to_html(self):
-        return (
-            self.value
-            if self.tag is None
-            else f'<{self.tag}{self.props_to_html()}>{self.value}<\\{self.tag}>'
-        )
+#     def to_html(self):
+#         return (
+#             self.value
+#             if self.tag is None
+#             else f'<{self.tag}{self.props_to_html()}>{self.value}<\\{self.tag}>'
+#         )
 
-    def __repr__(self) -> str:
-        return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
+#     def __repr__(self) -> str:
+#         return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
 
-if __name__ == '__main__':
-    node = LeafNode("test text 123", "p", {"test": "test", "test2": "test2"})
-    print(node)
-    print(node.to_html())
-    node2 = LeafNode("test text 123")
-    print(node2.to_html())
+# if __name__ == '__main__':
+#     node = LeafNode("test text 123", "p", {"test": "test", "test2": "test2"})
+#     print(node)
+#     print(node.to_html())
+#     node2 = LeafNode("test text 123")
+#     print(node2.to_html())

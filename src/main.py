3a0923f18057e38eb1e7685a160cfbbf61d43765
kuangyuwu <kuangyuwu95@gmail.com>
2024-06-12 19:21:47 -0500

    add func copy_static_to_public

--- src/main.py
+++ src/main.py
@@ -1,7 +1,33 @@
+import os
+import shutil
+
 from src.inline import extract_markdown_images, split_nodes_image
 from src.textnode import TextNode
 
+def copy_static_to_public() -> None:
+
+    def copy_files(src, dst) -> None:
+        print(f"Start copying {src} to {dst}")
+        items = os.listdir(src)
+        for item in items:
+            old_path = src + "/" + item
+            new_path = dst + "/" + item
+            if os.path.isfile(old_path):
+                print(f"Copy {old_path} to {new_path}")
+                shutil.copy(old_path, new_path)
+            elif os.path.isdir(old_path):
+                os.mkdir(new_path)
+                copy_files(old_path, new_path)
+        return
+
+    if os.path.exists("./public"):
+        shutil.rmtree("./public")
+    os.mkdir("./public")
+    copy_files("./static", "./public")
+    return
+
 def main() -> None:
+    copy_static_to_public()
     # node = TextNode("This is a text node", "bold", "https://www.boot.dev")
     # print(node)
     
